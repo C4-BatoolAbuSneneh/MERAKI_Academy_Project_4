a4da966a8800f203f9a9504a3aea3f870baeefdc
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-01-10 21:19:05 +0200

    add authentication to recipes.js

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -1,12 +1,13 @@
 const express = require("express")
-const { createNewRecipe, getAllRecipes,getRecipesById,updateRecipesById,deleteRecipeById} = require("../controllers/recipes")
+const { createNewRecipe, getAllRecipes,getRecipesById,updateRecipesById,deleteRecipeById} = require("../controllers/recipes");
+const authentication = require("../middleware/authentication");
 
 const recipesRouter = express.Router()
 
 
 
-recipesRouter.post("/", createNewRecipe);
-recipesRouter.get("/all", getAllRecipes);
+recipesRouter.post("/",authentication, createNewRecipe);
+recipesRouter.get("/all",authentication, getAllRecipes);
 recipesRouter.get("/search", getRecipesById);
 recipesRouter.put("/:id", updateRecipesById);
 recipesRouter.delete("/:id", deleteRecipeById);
