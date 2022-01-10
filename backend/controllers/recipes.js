ca7eca4fb20cd4656bca15feebd35346c912e675
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-01-10 17:52:17 +0200

    deleteRecipeById

--- backend/controllers/recipes.js
+++ backend/controllers/recipes.js
@@ -94,6 +94,27 @@ const createNewRecipe = (req, res) => {
         });
   };
   const deleteRecipeById = (req,res) => {
-
+    const id = req.params.id;
+    recipesModel
+      .findByIdAndDelete(id)
+      .then((result) => {
+        if (!result) {
+          return res.status(404).json({
+            success: false,
+            message: `The Recipe: ${id} is not found`,
+          });
+        }
+        res.status(200).json({
+          success: true,
+          message: `Succeeded to delete recipe with id: ${id}`,
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          success: false,
+          message: `Server Error`,
+        });
+      });
+        
   }
-module.exports = {createNewRecipe, getAllRecipes,getRecipesById,updateRecipesById};
+module.exports = {createNewRecipe, getAllRecipes,getRecipesById,updateRecipesById,deleteRecipeById};
--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { createNewRecipe, getAllRecipes,getRecipesById} = require("../controllers/recipes")
+const { createNewRecipe, getAllRecipes,getRecipesById,updateRecipesById,deleteRecipeById} = require("../controllers/recipes")
 
 const recipesRouter = express.Router()
 
@@ -8,5 +8,7 @@ const recipesRouter = express.Router()
 recipesRouter.post("/", createNewRecipe);
 recipesRouter.get("/all", getAllRecipes);
 recipesRouter.get("/search", getRecipesById);
+recipesRouter.put("/:id", updateRecipesById);
+recipesRouter.delete("/:id", deleteRecipeById);
 
 module.exports = recipesRouter
