6d3a6b3ec46918a7725d78358e036bc0a0f879cf
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-01-10 21:27:55 +0200

    add status to authentication function

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -2,18 +2,17 @@ const jwt = require("jsonwebtoken")
 
 const authentication = (req, res, next) => {
     if(!req.headers.authorization) {
-        res.json({ success:false, message: "forbidden"
+        res.status(403).json({ success:false, message: "forbidden"
      })
     }
     const token = req.headers.authorization.split(" ").pop();
     jwt.verify(token, process.env.SECRET, (err, result) => {
         if (err) {
-            res.json({success:false, message:"The token is invalid or expired"})
+            res.status(403).json({success:false, message:"The token is invalid or expired"})
         } else {
-           req.token = result
+           req.token = result;
            next()
           } 
-    
     })
 }
 module.exports = authentication
