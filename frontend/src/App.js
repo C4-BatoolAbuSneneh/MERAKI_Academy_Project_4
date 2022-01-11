8a11187b24dad4405f68b26dcc883a4d631bff2a
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-01-11 16:08:18 +0200

    import recipe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,18 @@ import { Routes, Route, Link } from "react-router-dom";
 import Navigation from "./component/Navigation";
 import Register from "./component/Register";
 import Login from "./component/Login";
+import Recipe from "./component/Recipe";
 function App() {
+  const [token, setToken] = useState("");
+  const [isLogedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div className="App">
-      <Navigation />
+      <Navigation isLogedIn={isLogedIn} />
       <Routes>
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path="/recipe" element={<Recipe token={token} />} />
       </Routes>
     </div>
   );
