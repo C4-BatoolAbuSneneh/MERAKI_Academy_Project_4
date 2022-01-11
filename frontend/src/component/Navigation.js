da1eb89173960239fd6dace753322bf5932b37ec
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-01-11 13:14:54 +0200

    create link login

--- frontend/src/component/Navigation.js
+++ frontend/src/component/Navigation.js
@@ -1,11 +1,17 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
-    return (
-        <>
-        </>
-    )
-}
+  return (
+    <>
+      <Link className="Link" to="/register">
+        Register
+      </Link>
+      <Link className="Link2" to="/login">
+        login
+      </Link>
+    </>
+  );
+};
 
 export default Navigation;
