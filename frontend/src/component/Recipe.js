d8992ac90bfa6f625b9e044550bab78ef0db7d5b
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-01-11 17:05:35 +0200

    edit function createRecipe

--- frontend/src/component/Recipe.js
+++ frontend/src/component/Recipe.js
@@ -1,30 +1,38 @@
 import React, {useState} from "react";
 import axios from "axios";
-const Recipe = () => {
-    const [image, setImage]  = useState("");
+// import burger from './image'
+const Recipe = ({token}) => {
+    
+    // const [image, setImage]  = useState("");
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const [time, setTime] = useState("");
+    const [paragraph, setParagraph] = useState("");
+
+    const createRecipe = () => {
+        axios.post("http://localhost:5000/articles", {title,description,time},{ headers: { Authorization: `Bearer ${token}` } 
+    }).then((response) => {
+        setParagraph("The recipe has been created successfully")
+    }).catch((err) => {
+        setParagraph("Error happened while creating a new recipe, please try again"
+        )
+    })
+    }
+    
     return (
 <>
 <div className="">
-<image  onChange={(e) => {
-    setImage(e.target.value)
-}} ></image>
+{/* <img src={burger} alt="burger" /> */}
 <input onChange={(e) => {
     setTitle(e.target.value)
 }} type="text" placeholder="write your title"></input>
 <textarea onChange={(e) => {
     setDescription(e.target.value)
-}} type="text" placeholder="write your title"></textarea>
+}} type="text" placeholder="write your description"></textarea>
 <input onChange={(e) => {
     setTime(e.target.value)
 }} type="text" placeholder="write your time"></input>
-<button onClick={()=> {
-axios.post("http://localhost:5000/recipes", {
-    image,title,description,time
-})
-}}>add new recipe</button>
+<button onClick={createRecipe}>create new recipe</button>
 
 
 </div>
