bba0d34067f8943d7fccba6533c86a0767240a3d
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-01-11 13:15:30 +0200

    import login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,18 @@
-import React from "react"
+import React from "react";
 import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import Navigation from "./component/Navigation";
-import Register from "./component/Register"
-
+import Register from "./component/Register";
+import Login from "./component/Login";
 function App() {
   return (
     <div className="App">
-      <Navigation/>
+      <Navigation />
       <Routes>
-      <Route path="/register" element={<Register />} />
-      <Route/>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
 }
-
 export default App;
