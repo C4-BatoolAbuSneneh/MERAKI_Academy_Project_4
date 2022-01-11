cdd7e74c0ab9b8a04b678ec4e8d14734576b0ef7
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-01-11 11:13:15 +0200

    add authorization to recipes.js(route)

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -1,15 +1,16 @@
 const express = require("express")
 const { createNewRecipe, getAllRecipes,getRecipesById,updateRecipesById,deleteRecipeById} = require("../controllers/recipes");
 const authentication = require("../middleware/authentication");
+const authorization = require("../middleware/authorization")
 
 const recipesRouter = express.Router()
 
 
 
-recipesRouter.post("/",authentication, createNewRecipe);
+recipesRouter.post("/",authentication,authorization, createNewRecipe);
 recipesRouter.get("/all",authentication, getAllRecipes);
-recipesRouter.get("/search", getRecipesById);
-recipesRouter.put("/:id", updateRecipesById);
-recipesRouter.delete("/:id", deleteRecipeById);
+recipesRouter.get("/search",authorization, getRecipesById);
+recipesRouter.put("/:id", authorization,updateRecipesById);
+recipesRouter.delete("/:id",authorization, deleteRecipeById);
 
 module.exports = recipesRouter
