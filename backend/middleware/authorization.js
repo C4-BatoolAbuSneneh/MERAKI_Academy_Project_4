418f13f46749d2ee22289023fc48fa18c0899277
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-01-11 11:11:41 +0200

    add authorization middleware

---
+++ backend/middleware/authorization.js
@@ -0,0 +1,13 @@
+const authorization = (string) => {
+return (req, res, next) => {
+    if (req.token.role == "ADMIN") {
+        next()
+    } else {
+       res.status(403).json({
+           success: false,
+            message: "Unauthorized"
+       })
+    }
+}    
+}
+module.exports = authorization
