93c773fed409cd523f398cbec427ca4a05cdb5fc
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-01-12 23:17:47 +0200

    create setAdmin,setLoggedin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,47 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./component/Navigation";
 import Register from "./component/Register";
 import Login from "./component/Login";
-import Recipes from "./component/Recipes"
+import Recipes from "./component/Recipes";
 import AllRecipe from "./component/AllRecipes";
+import MyFavourite from "./component/MyFavourite";
 function App() {
-  const [token, setToken] = useState("");
-  const [isLogedIn, setIsLoggedIn] = useState(false);
+  let [token, setToken] = useState("");
+  token = localStorage.getItem("token");
 
+  const [isLogedIn, setIsLoggedIn] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
   return (
     <div className="App">
-      <Navigation isLogedIn={isLogedIn} />
+      <Navigation
+        isLogedIn={isLogedIn}
+        isAdmin={isAdmin}
+        setIsAdmin={setIsAdmin}
+        setIsLoggedIn={setIsLoggedIn}
+      />
       <Routes>
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} setToken={setToken} />} />
-        <Route path="/recipes" element={<Recipes token={token} />} />
-        <Route path="/all" element={<AllRecipe token={token} />} />
-
+        <Route
+          path="/login"
+          element={
+            <Login
+              setIsLoggedIn={setIsLoggedIn}
+              setIsAdmin={setIsAdmin}
+              setToken={setToken}
+            />
+          }
+        />
+        <Route
+          path="/recipes"
+          element={isAdmin ? <Recipes token={token} /> : <p>Not Admin</p>}
+        />
+        <Route
+          path="/all"
+          element={<AllRecipe token={token} isAdmin={isAdmin} />}
+        />
+        <Route path="/my" element={<MyFavourite token={token} />} />
       </Routes>
     </div>
   );
