786b8c7daec710ad4ec005c7a5174db8b06806ce
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-01-13 17:22:14 +0200

    create about component

---
+++ frontend/src/component/About.js
@@ -0,0 +1,24 @@
+import React from "react";
+const About = () => {
+  return (
+    
+    <>
+      <p
+        style={{
+          color: "black",
+          fontSize: "30px",
+          fontFamily: "sans-serif",
+          textAlign: "start",
+        }}
+      > 
+        DON'T WAIT TO <span style={{ fontFamily: "fantasy" }}>COOK</span> YOUR{" "}
+        <br />{" "}
+        <span style={{ fontSize: "40px", fontFamily: "Fantasy" }}>
+          FAVOURITE DISH
+        </span>{" "}
+      </p>
+    </>
+  );
+};
+
+export default About;
