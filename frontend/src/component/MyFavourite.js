2cbd778c86477d291faea85e0962d945aedae715
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-01-13 17:23:00 +0200

    create my favorite  component

--- frontend/src/component/MyFavourite.js
+++ frontend/src/component/MyFavourite.js
@@ -1,9 +1,27 @@
 import React from "react";
- const MyFavourite = () => {
-return(
+import axios from "axios";
+const MyFavourite = () => {
+  //   const handelRemove = () => {
+  //     localStorage.removeItem("favourite");
+  //   };
+  const favourites = JSON.parse(localStorage.getItem("favourite"));
+  return (
     <>
-
-</>
-    )
- }
-export default MyFavourite
+      <div className="myfavourite">
+        {favourites && favourites.map((ele) => {
+          return (
+            <>
+              <img style={{ width: "350px" }} src={ele.image} />
+              <p className="title"> {ele.title}</p>
+              <p className="time"> {ele.time}</p>
+              <p className="ingredient"> Ingredients : {ele.ingredients}</p>
+              <p className="description"> Description: {ele.description}</p>
+              <button>delete</button>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+export default MyFavourite;
