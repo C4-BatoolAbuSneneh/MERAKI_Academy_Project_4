54f7813d8403b2be20bdc2c06ed2ec53c6ac289b
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-01-16 19:55:53 +0200

    create function getProductById in backend

--- backend/controllers/recipes.js
+++ backend/controllers/recipes.js
@@ -1,120 +1,145 @@
-const recipesModel = require("../database/models/recipes")
+const recipesModel = require("../database/models/recipes");
 
 const createNewRecipe = (req, res) => {
-    const {image,title, ingredients,description ,time} = req.body;
-    const newRecipe = new recipesModel({
-      image,
-      title,
-      ingredients,
-      description,
-      time,
-    });
-    newRecipe
+  const { image, title, ingredients, description, time } = req.body;
+  const newRecipe = new recipesModel({
+    image,
+    title,
+    ingredients,
+    description,
+    time,
+  });
+  newRecipe
     .save()
     .then((result) => {
-        res.status(201).json({
-            success:true,
-            message: `recipe created`,
-            result: result,
-        })
+      res.status(201).json({
+        success: true,
+        message: `recipe created`,
+        result: result,
+      });
     })
-        .catch((err) => {
-            res.status(500).json({
-                success:false,
-                message:`server error`,
-                err: err,
-        })
-    })
-  }
-  const getAllRecipes = (req,res) => {  
-      recipesModel
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `server error`,
+        err: err,
+      });
+    });
+};
+const getAllRecipes = (req, res) => {
+  recipesModel
     .find({})
     .then((recipes) => {
-        res.status(200).json({
-           success: true,
-           message: `All the recipes`,
-           recipes: recipes
-        })
-    })
-        .catch(err => {
-            res.status(500).json({
-                success: false,
-                message: `No recipe yet`,
-                err:err
-            
-        })
-    })
-  }
-  const getRecipesById = (req,res) => {
-      let id = req.query.id;
-      recipesModel
-      .findById(id)
-      .then((result) => {
-         if (!result) {
-             return res.status(404).json({
-                 success: false,
-                 message: `The Recipe not found`
-             })
-         } res.status(200).json({
-             success: true,
-             message: `The recipe ${id}`,
-             result: result
-         });
-         })
-         .catch((err) => {
-             res.status(500).json({
-                 success:false,
-                 message: `Server Error`
-             })
-         })
-  }
-  const updateRecipesById = (req,res) => {
-      const recipe = req.params.id;
-      let id  = req.body
-      recipesModel
-      .findByIdAndUpdate({_id:recipe}, id , { new: true })
-      .then((result) => {          
-        if (!result) {
-            return res.status(404).json({
-                success: false,
-                message: `The Recipe ${_id} is not found`
-            })
-        }
-         res.status(202).json({
-            success: true,
-            message: `recipe updated`,
-            recipe: result
-        });
-        })
-        .catch((err) => {
-            res.status(500).json({
-                success:false,
-                message: `Server Error`
-            });
-        });
-  };
-  const deleteRecipeById = (req,res) => {
-    const id = req.params.id;
-    recipesModel
-      .findByIdAndDelete(id)
-      .then((result) => {
-        if (!result) {
-          return res.status(404).json({
-            success: false,
-            message: `The Recipe: ${id} is not found`,
-          });
-        }
-        res.status(200).json({
-          success: true,
-          message: `Succeeded to delete recipe with id: ${id}`,
-        });
-      })
-      .catch((err) => {
-        res.status(500).json({
-          success: false,
-          message: `Server Error`,
-        });
+      res.status(200).json({
+        success: true,
+        message: `All the recipes`,
+        recipes: recipes,
       });
-        
-  }
-module.exports = {createNewRecipe, getAllRecipes,getRecipesById,updateRecipesById,deleteRecipeById};
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `No recipe yet`,
+        err: err,
+      });
+    });
+};
+const getRecipesById = (req, res) => {
+  let id = req.query.id;
+  recipesModel
+    .findById(id)
+    .then((result) => {
+      if (!result) {
+        return res.status(404).json({
+          success: false,
+          message: `The Recipe not found`,
+        });
+      }
+      res.status(200).json({
+        success: true,
+        message: `The recipe ${id}`,
+        result: result,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+      });
+    });
+};
+const updateRecipesById = (req, res) => {
+  const recipe = req.params.id;
+  let id = req.body;
+  recipesModel
+    .findByIdAndUpdate({ _id: recipe }, id, { new: true })
+    .then((result) => {
+      if (!result) {
+        return res.status(404).json({
+          success: false,
+          message: `The Recipe ${_id} is not found`,
+        });
+      }
+      res.status(202).json({
+        success: true,
+        message: `recipe updated`,
+        recipe: result,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+      });
+    });
+};
+const deleteRecipeById = (req, res) => {
+  const id = req.params.id;
+  recipesModel
+    .findByIdAndDelete(id)
+    .then((result) => {
+      if (!result) {
+        return res.status(404).json({
+          success: false,
+          message: `The Recipe: ${id} is not found`,
+        });
+      }
+      res.status(200).json({
+        success: true,
+        message: `Succeeded to delete recipe with id: ${id}`,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+      });
+    });
+};
+const getProductById = (req, res) => {
+  const id = req.params.id;
+    recipesModel
+    .findById(id)
+    .then((result) => {
+      res.status(404).json({
+        success: true,
+        message: `the recipe ${id}`,
+        result: result
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message:`server error`,
+       error:err
+      });
+    });
+};
+module.exports = {
+  createNewRecipe,
+  getAllRecipes,
+  getRecipesById,
+  updateRecipesById,
+  deleteRecipeById,
+  getProductById
+};
--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -5,6 +5,7 @@ const {
   getRecipesById,
   updateRecipesById,
   deleteRecipeById,
+  getProductById
 } = require("../controllers/recipes");
 const authentication = require("../middleware/authentication");
 const authorization = require("../middleware/authorization");
@@ -16,5 +17,6 @@ recipesRouter.get("/all", authentication, getAllRecipes);
 recipesRouter.get("/search", authentication, getRecipesById);
 recipesRouter.put("/:id", authentication, authorization, updateRecipesById);
 recipesRouter.delete("/:id", authentication, authorization, deleteRecipeById);
+recipesRouter.get("/all/product/:id", authentication, getProductById);
 
 module.exports = recipesRouter;
