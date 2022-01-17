bb81730f0e680a9ab86537903edf46326e3a508f
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-01-17 11:30:32 +0200

    use useParams to on click the recipe show it all description and ingredient

--- frontend/src/component/Product/Product.js
+++ frontend/src/component/Product/Product.js
@@ -1,48 +1,55 @@
 import React, { useState, useEffect } from "react";
+import "./Product.css";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
-const Product = ({ tokenn }) => {
-
+const Product = ({ token }) => {
+  const { id } = useParams();
   const [product, setProduct] = useState([]);
+
   useEffect(() => {
-    getProductById();
+    getProductById(id);
   }, []);
 
   const getProductById = (id) => {
     axios
       .get(`http://localhost:5000/recipes/all/product/${id}`, {
-        headers: { Authorization: `Bearer ${tokenn}` },
+        headers: { Authorization: `Bearer ${token}` },
       })
-      .then((result) => {
-          console.log(result);
-        setProduct(result.data.product);
+      .then((response) => {
+        setProduct(response.data.result);
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
-//   const Product = () => {
-//     const { id } = useParams();       
-//     return <div>id: {id}</div>;
-//   };
-  const myProduct =
-    product &&
-    product.map((ele, i) => {
-      return (
-        <>
-          <div key={i} className="product">
-            <img style={{ width: "50%" }} src={ele.image} />
-            <p className="title"> {ele.title}</p>
-            <p className="time"> {ele.time}</p>
-            <p className="ingredient"> Ingredients : {ele.ingredients}</p>
-            <p className="description"> Description: {ele.description}</p>
-          </div>
-        </>
-      );
-    });
-  return <>{myProduct}</>;
+  return (
+    <>
+      <br />
+      <br />
+      <div className="product">
+        <img style={{ width: "40%" }} src={product.image} />
+        <br />
+        <p style={{ fontSize: "2.5em", color: "black" }}> {product.title}</p>
+        <p style={{ fontSize: "2.5em" }} className="time">
+          {" "}
+          {product.time}
+        </p>
+        <p style={{ fontSize: "2.5em" }} className="ingredient">
+          {" "}
+          Ingredients : {product.ingredients}
+        </p>
+        <p
+          style={{ fontSize: "2.5em", color: "black" }}
+          className="description"
+        >
+          {" "}
+          Description: {product.description}
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default Product;
